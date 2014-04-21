3dc4a8d46a43e046914fb86979cc0145f379db25
Matt Burke <burkemd1+github@gmail.com>
2014-04-21 12:51:29 -0400

    Add mysqldump task (needs refactoring)

--- lib/tasks/mysql.rake
+++ lib/tasks/mysql.rake
@@ -5,4 +5,11 @@ namespace :mysql do
     config = RailsMysql::DatabaseConfig.from_yaml(Rails.env)
     RailsMysql::CliCommand.new(config).execute
   end
+
+  desc "dumps to a timestamped file"
+  task :dump do
+    config = RailsMysql::DatabaseConfig.from_yaml(Rails.env)
+    Kernel.exec("mysqldump -h \"#{config.host}\" -u \"#{config.username}\" -p \"#{config.password}\" -P \"#{config.port}\"")
+  end
+
 end
--- spec/lib/tasks/mysql_rake_spec.rb
+++ spec/lib/tasks/mysql_rake_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 require 'rails'
 
 def rake_path
-  [File.join(%W{ lib tasks })]
+  [File.expand_path(File.join(%W{ lib tasks }))]
 end
 
 def rake(task)
   Rake.application[task].invoke
 end
 
+def fixture_path(fixture)
+  File.expand_path(File.dirname(__FILE__) + "../../../fixtures/#{fixture}")
+end
+
 def with_fixture(fixture)
   old_path = Dir.pwd
-  Dir.chdir "spec/fixtures/#{fixture}"
+  Dir.chdir fixture_path(fixture)
   yield
 ensure
   Dir.chdir old_path
@@ -19,10 +23,9 @@ end
 
 
 describe 'rake tasks' do
-  let(:rake_app) { Rake::Application.new }
 
-  before do
-    Rake.application = rake_app
+  before(:all) do
+    Rake.application = Rake::Application.new
     Rake.application.rake_require 'mysql', rake_path
   end
 
@@ -33,8 +36,30 @@ describe 'rake tasks' do
     it 'calls exec with the correct params' do
       with_fixture("default") do
         rake 'mysql:cli'
-        expect(Kernel).to have_received(:exec).with("mysql -h\"HOST\" -u\"USER\" -p\"PASSWORD\" -P\"PORT\" -D\"DATABASE\"");
       end
+
+      expect(Kernel).to have_received(:exec).with("mysql -h\"HOST\" -u\"USER\" -p\"PASSWORD\" -P\"PORT\" -D\"DATABASE\"");
+
+    end
+  end
+
+  describe 'rake mysql:dump' do
+    before { Kernel.stub(:exec) }
+    before { Rails.stub(:env) { "development" } }
+
+    it 'calls exec with the correct params' do
+      expect(Kernel).to receive(:exec) do |cmd|
+        expect(cmd).to match(/^mysqldump\b/)
+        expect(cmd).to match(/-h\s+"HOST"/)
+        expect(cmd).to match(/-u\s+"USER"/)
+        expect(cmd).to match(/-p\s+"PASSWORD"/)
+        expect(cmd).to match(/-P\s+"PORT"/)
+        expect(cmd).to match(/|\s+>\s+.*/) # cats to some file
+      end
+      with_fixture("default") do
+        rake 'mysql:dump'
+      end
+
     end
   end
 end
